b399dab39e55d452f1b912526c7eec3f06bcffa5
Matt Farmer <mrfarmer777@gmail.com>
2018-06-17 01:22:31 +0000

    automatically backed up by learn

--- db/migrate/20180617012041_create_movies.rb
+++ db/migrate/20180617012041_create_movies.rb
@@ -1,4 +1,11 @@
 class CreateMovies < ActiveRecord::Migration
   def change
+    create_table :movie do |i|
+      i.string :title
+      i.datetime :release_date
+      i.string :director
+      i.string :lead
+      i.boolean :in_theaters
+    end
   end
 end
